f59cc33a5c7c6784c6f72ac30a3aa80eb2f444a7
John Tran <jtran-csula@pcwerk.com>
2015-02-14 14:09:54 -0800

    updated pom.xml to support exec:java modified main application to take an argument

--- src/main/java/edu/calstatela/cs454/jtran/firstsearch/App.java
+++ src/main/java/edu/calstatela/cs454/jtran/firstsearch/App.java
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	try { 
+    		String name = args[0];
+    		System.out.println( "Hello " + name + "!" );
+    	} catch (Exception ex){
+    		System.out.println( "Hello World!" );
+    	}
     }
 }
